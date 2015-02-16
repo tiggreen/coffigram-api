d893a54522ad4eeb1ea95507a7b015128529220d
Tigran Hakobyan <tik.hakobyan@gmail.com>
2015-02-16 16:08:07 -0500

    Final version 1.0 is up.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * Module dependencies.
  */
 
+require('newrelic');
 var express = require('express');
 Instagram = require('instagram-node-lib');
 var bodyParser = require('body-parser');
@@ -18,6 +19,12 @@ var foursquare = require('node-foursquare-venues')
                 (process.env.FOURSQUARE_CLIENT_ID, 
                  process.env.FOURSQUARE_CLIENT_SECRET)
 
+var yelp = require("yelp").createClient({
+  consumer_key: process.env.YELP_CONSUMER_KEY,
+  consumer_secret: process.env.YELP_CONSUMER_SECRET ,
+  token: process.env.YELP_TOKEN,
+  token_secret: process.env.YELP_TOKEN_SECRET
+});
 
 
 var app = module.exports = express();
@@ -66,58 +73,115 @@ var apiKeys = [process.env.API_KEY];
 // we now can assume the api key is valid,
 // and simply expose the data
 app.get('/api', function(req, res, next){
-  res.send('Hello this is Coffigram API speaking!');
+  res.send('Hello this is Coffigram api speaking!');
 });
 
-var calc = function(num, callback) {
-  return callback(null, num * num);
-}
 
-// try to match the foursquare place to a instragram place.
+/*
+* Match foursquare venue to the instagram location. 
+* 
+*/
 var get_instagram_loc_id = function(venue, callback) {
-  //console.log(venue);
-  
   Instagram.locations.search({ 
     foursquare_v2_id: venue.id,
     complete: function(data){
       return callback(null, data);
-    }, 
-    error: function(errorMessage, errorObject, caller) {
-      console.log(errorMessage);
-      return callback(errorObject, null);
-        // errorMessage is the raised error message
-        // errorObject is either the object that caused the issue, or the nearest neighbor
-        // caller is the method in which the error occurred
     }
+    // error: function(errorMessage, errorObject, caller) {
+    //   console.log(errorMessage);
+    //   return callback(errorObject, null);
+    //     // errorMessage is the raised error message
+    //     // errorObject is either the object that caused the issue, or the nearest neighbor
+    //     // caller is the method in which the error occurred
+    // }
   });
 }
 
+/*
+* Get a location ID and return all the media
+* items that were taken in that place.
+*/
 var get_media = function(place, callback_media) {
-
   Instagram.locations.recent({ 
     location_id: place[0].id,
     complete: function(data, pagination){
-      // we should return pagination instead so the result set is smaller.
       return callback_media(null, pagination);
     }, 
-    error: function(errorMessage, errorObject, caller) {
-      console.log(errorMessage);
-      return callback_media(errorObject, null);
-        // errorMessage is the raised error message
-        // errorObject is either the object that caused the issue, or the nearest neighbor
-        // caller is the method in which the error occurred
-    }
+    // error: function(errorMessage, errorObject, caller) {
+    //   console.log(errorMessage);
+    //   return callback_media(errorObject, null);
+    // }
   });
 }
 
+/*
+* Get info for a vanue. Info consists of Yelp and Foursquare
+* data. We need this for getting the ratings of the place.
+*/
+var get_venue_info = function(venue, callback_info) {
+  foursquare.venues.venue(venue.id, {}, function(err, data) {
+    if (! err) {
+      var lat = venue.location.lat;
+      var lng = venue.location.lng;
+      yelp.search({
+                   ll: lat + "," + lng, 
+                   term: venue.name,
+                   limit: 1
+                 }, function(error, ydata) {
+        if (! error && ydata) { 
+          var ret_data = {fq: data.response.venue, yelp: ydata};
+          return callback_info(null, ret_data)
+        } else {
+          return callback_info(null, {fq: data.response.venue})
+        }
+      });
+    } else return callback_info(err, null)
+
+  })
+}
+
+/*
+* Get all the photos with their info of NYC. 
+* 
+*/
 app.get('/api/get_nyc_photos', function(req, res, next) {
-  
   foursquare.venues.search({
     near: 'New York City, NY',
     categoryId: '4bf58dd8d48988d1e0931735',
     limit: 50,
   }, function(err, data) {
+    if (! err) {
+      var venues = data.response.venues;
+      async.map(venues, get_instagram_loc_id, function(error, places){
+        if (! error  && places) {
+          // at this point we have bunch of instagram coffee shops (places).
+          async.map(places, get_media, function(err_pics, pics){
+            if (! err_pics && pics) {
+              async.map(venues, get_venue_info, function(err_venues, info){
+                if (! err_venues && info) {
+                  res.send({'pics':pics, 'venues':info});
+                } else next();
+              });
+            } else next();
+          });
+        } else next();
+      });
 
+    } else next();
+  })
+});
+
+
+/*
+* Get all the photos with their info of SF. 
+* 
+*/
+app.get('/api/get_sf_photos', function(req, res, next) {
+  foursquare.venues.search({
+    near: 'San Francisco, CA',
+    categoryId: '4bf58dd8d48988d1e0931735',
+    limit: 50,
+  }, function(err, data) {
     if (! err) {
       var venues = data.response.venues;
       async.map(venues, get_instagram_loc_id, function(err, places){
@@ -125,36 +189,17 @@ app.get('/api/get_nyc_photos', function(req, res, next) {
           // at this point we have bunch of instagram coffee shops (places).
           async.map(places, get_media, function(err, pics){
             if (! err && pics) {
-              // at this point we have an array of pics
-              res.send({'pics':pics, 'venues':venues});
+              async.map(venues, get_venue_info, function(err, info){
+                if (! err && info) {
+                  res.send({'pics':pics, 'venues':info});
+                }
+              });
             }
           });
         }
       });
 
     } else next();
-  })
-
-});
-
-app.get('/api', function(req, res, next){
-  res.send('Hello this is the coffee API speaking now!');
-});
-
-app.get('/api/get_sf_photos', function(req, res, next) {
-  foursquare.venues.search({
-    near: 'San Francisco, CA',
-    categoryId: '4bf58dd8d48988d1e0931735'
-  }, function(err, data) {
-
-    if (! err) {
-      var venues = data.response.venues;
-      res.status(200).send(venues);
-      // var venues = body.response.venues;
-      // res.status(200).send(venues);
-
-    } else next();
-
   })
 });
 
